8d686f6a0b82ce3ca70f39cdb0f82434bd7266d0
marhamatabadi <marhamatabadi@GMAIL.COM>
2018-10-28 15:17:52 +0330

    eff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 class App extends Component {
+    componentWillMount() {
+      
+        
+    }
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>
-                    An App
-                </Text>
+                <LoginForm />
             </View>
         );
     }
